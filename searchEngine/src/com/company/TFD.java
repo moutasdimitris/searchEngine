a0d755da28ed9032bfa053ef7df7c42772475c22
Dimitrios Moutafidis <dmoutafi@csd.auth.gr>
2020-01-07 18:16:49 +0200

    Update v5

---
+++ searchEngine/src/com/company/TFD.java
@@ -0,0 +1,33 @@
+package com.company;
+
+ class TFD {
+        String text;
+        String doc_id;
+        Integer freq;
+
+        String getText() {
+            return text;
+        }
+
+        void setText(String text) {
+            this.text = text;
+        }
+
+        String getDoc_id() {
+            return doc_id;
+        }
+
+        void setDoc_id(String doc_id) {
+            this.doc_id = doc_id;
+        }
+
+        Integer getFreq() {
+            return freq;
+        }
+
+        void setFreq(Integer freq) {
+            this.freq = freq;
+        }
+
+
+}
