3409ed7e04aba0f5361d2e430f5c31d2706d8b26
Dimitrios Moutafidis <dmoutafi@csd.auth.gr>
2020-01-21 02:07:23 +0200

    V 1

---
+++ CrawlerIndexer/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        HashSet<String> words=new HashSet<>();//All words
+        HashSet<String> links;//All links
+        HashSet<HashSet<TFD>> sets;//All (w,d,f)
+        Crawler cr = new Crawler();
+        cr.crawling(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), Boolean.parseBoolean(args[3]));
+        links=cr.getLinks();
+
+        Indexer index = new Indexer();
+        index.clean_html(links);
+        sets=index.getHash();
+
+        for (HashSet<TFD> s : sets) {
+            for (TFD tfd:s){
+                words.add(tfd.getText());
+             }
+        }
+        for (HashSet<TFD> s : sets) {
+            for (TFD tfd:s){
+        System.out.println(tfd.getText()+" "+tfd.getDoc_id()+" "+tfd.getFreq());
+            }
+        }
+    }
+}
+
